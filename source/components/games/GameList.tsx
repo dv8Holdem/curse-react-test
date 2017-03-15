0dd546919b50a4d988c54abdd838e258aabfac37
Robert Cruz <robert.cruz@emc.com>
2017-03-15 15:09:32 -0700

    Added search

--- source/components/games/GameList.tsx
+++ source/components/games/GameList.tsx
@@ -3,11 +3,10 @@ import { Router } from 'react-router';
 import {lodash} from 'lodash';
 import {Game} from '../../models/Game';
 export interface GameListProps extends React.Props<GameList> {
-    // Define any props taken by List itself.
+
 }
 
 export interface ConnectedProps {
-    // Define any connected props here. (The ones mapped by ListContainer.)
     games: Object,
     isFetching: boolean,
     error: string,
@@ -16,7 +15,6 @@ export interface ConnectedProps {
 }
 
 export interface ConnectedDispatch {
-    // Define any connected dispatch actions here. (The ones mapped by ListContainer.)
     getGames:()=>void,
     isFetching:()=>void
 }
@@ -24,19 +22,46 @@ export interface ConnectedDispatch {
 type CombinedTypes = GameListProps & ConnectedProps & ConnectedDispatch;
 
 export class GameList extends React.Component<CombinedTypes, void> {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchQuery:'',
+            currentList:this.props.games
+        }
 
+        this.onSearchInputChange = this.onSearchInputChange.bind(this);
+    }
     static contextTypes: React.ValidationMap<any> = {
         router: React.PropTypes.object
     };
 
+    onSearchInputChange(event){
+        //let newList = _.filter(this.props.games, game => game.Name.contains(event.target.value.toLowerCase()));
+        let newList = _.filter(this.props.games, function(game){
+            let name = game.Name.toLowerCase();
+            return game.Name.toLowerCase().indexOf(event.target.value.toLowerCase()) >-1;
+        });
+
+        this.setState({
+            searchQuery:event.target.value,
+            currentList:newList
+        })
+
+        console.log('this.state:', this.state)
+    }
+
     componentDidMount() {
         if(!this.props.isLoaded){
-          this.props.getGames();
+            this.props.getGames().then(()=>{
+                this.setState({
+                    currentList:this.props.games
+                })
+            });
         }
-
     }
+
     viewDetails(gameID:string){
-      this.context.router.push('game/'+gameID);
+        this.context.router.push('game/'+gameID);
     }
 
     render() {
@@ -46,30 +71,31 @@ export class GameList extends React.Component<CombinedTypes, void> {
 
         if(this.props.isLoaded){
             return <div className='GameList--root'>
-              <table>
-                  <thead>
-                      <tr>
-                          <th>Game</th>
-                          <th>Game Id</th>
-                          <th>Game Name</th>
-                          <th>Support Addons</th>
-                          <th>Support Voice</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    {this.props.games.map((game:Game) => (
-                      
-                        <tr key={game.ID} onClick={()=>this.viewDetails(game.ID)}>
-                          <td>{game.ID}</td>
-                          <td>{game.ID}</td>
-                          <td>{game.Name}</td>
-                          <td>{game.SupportsAddons.toString()}</td>
-                          <td>{game.SupportsVoice.toString()}</td>
+                <input type="text" value={this.state.searchQuery} onChange={this.onSearchInputChange} />
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Game</th>
+                            <th>Game Id</th>
+                            <th>Game Name</th>
+                            <th>Support Addons</th>
+                            <th>Support Voice</th>
                         </tr>
-                    
+                    </thead>
+                    <tbody>
+                    {this.state.currentList.map((game:Game) => (
+
+                        <tr key={game.ID} onClick={()=>this.viewDetails(game.ID)}>
+                            <td>{game.ID}</td>
+                            <td>{game.ID}</td>
+                            <td>{game.Name}</td>
+                            <td>{game.SupportsAddons.toString()}</td>
+                            <td>{game.SupportsVoice.toString()}</td>
+                        </tr>
+
                     ))}                  
-                  </tbody>
-              </table>
+                    </tbody>
+                </table>
             </div> 
         }
         return (
