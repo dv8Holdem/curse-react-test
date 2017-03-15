4f82c320393809108a3c61a2e1957375e2648b60
dv8Holdem <dv8.cruz@gmail.com>
2017-03-14 23:11:30 -0700

    More Changes

--- source/components/games/GameDetails.tsx
+++ source/components/games/GameDetails.tsx
@@ -8,20 +8,43 @@ export interface GameDetailsProps extends React.Props<GameDetails> {
 
 export interface ConnectedProps {
     // Define any connected props here. (The ones mapped by ListContainer.)
-
+    games: Object,
+    isFetching: boolean,
+    error: string,
+    isLoaded: boolean,
+    selectedGame: Object
 }
 
 export interface ConnectedDispatch {
     // Define any connected dispatch actions here. (The ones mapped by ListContainer.)
+    getGames:(),
+    isFetching:()
 }
-
 type CombinedTypes = GameDetailsProps & ConnectedProps & ConnectedDispatch;
 
 export class GameDetails extends React.Component<CombinedTypes, void> {
     componentDidMount() {
-	console.log("this.props.params:", this.props);
+        if(this.props.isLoaded){
+            console.log("no games")
+            this.getDetails()
+        }else{
+            console.log("no games else")
+
+            this.props.getGames().then(()=>{
+                this.getDetails()
+            })       
+        }
+	    console.log("this.props.params:", this.props);
+        var id=this.props.params.id;
+        let game = _.find(this.props.games, function(game) { return game.ID == id; });
+        console.log("game : ", game)
+
     }
     
+    getDetails(){
+        var id=this.props.params.id;
+        let game = _.find(this.props.games, function(game) { return game.ID == id; });
+    }
 
     render() {
         return (
--- source/components/games/GameDetailsContainer.ts
+++ source/components/games/GameDetailsContainer.ts
@@ -2,18 +2,24 @@
 import * as React from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import { fetchGames } from '../../actions/games'
 import { GameDetails, GameDetailsProps, ConnectedProps, ConnectedDispatch } from './GameDetails';
 import { GlobalState } from '../../state/GlobalState';
 import {gamesReducer} from '../../reducers/gamesReducer';
 
 function mapStateToProps(state: GlobalState, props: GameDetailsProps): ConnectedProps {
     return {
-
+    	games: state.games.games,
+    	isFetching: state.games.isFetching,
+    	isLoaded: state.games.isLoaded,
+    	error:state.games.error,
+    	selectedGame:state.games.selectedGame
     }
 };
 
 function mapDispatchToProps(dispatch: Dispatch<any>): ConnectedDispatch {
     return bindActionCreators({ 
+        getGames: fetchGames
     }, dispatch);
 };
 
